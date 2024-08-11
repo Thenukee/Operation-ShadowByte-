fb1a95f9ef2a5bbce2902acfe61e401c2e7723ae
2k2TharushaBandara <123946740+2k2TharushaBandara@users.noreply.github.com>
2024-08-11 11:28:50 +0530

    Added Cytoscape.js test files

---
+++ #Tharu-Cytoscape tests/CytoscapeTestWebApp/CytoscapeTestWebApp/Views/Graph/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CytoscapeTestWebApp.Views.Graph
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
